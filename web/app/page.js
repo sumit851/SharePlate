06381428e9ae226a982d4ea90e70a6a33434a38d
Sumit Dalai <dalaisumit611@gmail.com>
2024-11-23 21:35:41 +0530

    initial commit

---
+++ web/app/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-green-400 to-blue-500 text-white font-bold text-xl">
+      Local Food Tracking Network
+    </div>
+  );
+};
+
+export default page;
